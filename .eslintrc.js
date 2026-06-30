5a76960c0cc92c2ff059326c7c842fb822210acd
Matin Rahmati Nezhad <matinrahmatinezhad@gmail.com>
2026-06-30 18:12:24 +0100

    update linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,11 @@ module.exports = {
     ecmaVersion: 'latest'
   },
   rules: {
+    semi: ['error', 'never'],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    eqeqeq: ['error', 'always']
   }
 }
